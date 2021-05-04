9a6368e86392e985fac5e1b1a14a59c29c3e653a
ToonDiscord <80784268+ToonDiscord@users.noreply.github.com>
2021-05-03 20:13:55 -0700

    v12

---
+++ uptime.js
@@ -0,0 +1,30 @@
+const { MessageEmbed } = require("discord.js");
+const config = require("../../botconfig/config.json");
+const ee = require("../../botconfig/embed.json");
+const { duration } = require("../../handlers/functions")
+module.exports = {
+    //change everything here to your choosing
+    name: "uptime",
+    category: "Information",
+    cooldown: 5,
+    usage: "uptime",
+    description: "Returns the duration on how long the Bot is online",
+    //dont touch anything here unless you know what you're doing
+    run: async (client, message, args, user, text, prefix) => {
+    try{
+      message.channel.send(new MessageEmbed()
+        .setColor(ee.color)
+        .setFooter(ee.footertext, ee.footericon)
+        .setTitle(`:white_check_mark: **${client.user.username}** is since:\n ${duration(client.uptime)} online`)
+      );
+    } catch (e) {
+        console.log(String(e.stack).bgRed)
+        return message.channel.send(new MessageEmbed()
+            .setColor(ee.wrongcolor)
+            .setFooter(ee.footertext, ee.footericon)
+            .setTitle(`❌ ERROR | An error occurred`)
+            .setDescription(`\`\`\`${e.stack}\`\`\``)
+        );
+    }
+  }
+}
